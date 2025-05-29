5109c0f4e948aab5094e2c88fd510ceb387c7d8d
Kae <kaehehehe@gmail.com>
2025-05-29 11:30:33 +0900

    feat: add playground

---
+++ playground/main.ts
@@ -0,0 +1,6 @@
+import vc from "../src/index";
+
+vc.logWithTheme({ message: "Success log!", theme: "success" });
+vc.logWithTheme({ message: "Info log!", theme: "info" });
+vc.logWithTheme({ message: "Warning log!", theme: "warn" });
+vc.logWithTheme({ message: "Error log!", theme: "error" });
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import path from "path";
+
+export default defineConfig({
+  root: path.resolve(__dirname, "playground"),
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "VisualConsole",
+      formats: ["es"],
+      fileName: "visual-console",
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
+  plugins: [dts()],
+});
