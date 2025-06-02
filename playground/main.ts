27a2b6919a717018de50a1e0e41c3f353409e6bf
Kae <kaehehehe@gmail.com>
2025-06-02 09:40:41 +0900

    chore: remove playground HTML and main TypeScript files

--- playground/main.ts
+++
@@ -1,6 +0,0 @@
-import vc from "../src/index";
-
-vc.logWithTheme({ message: "Success log!", theme: "success" });
-vc.logWithTheme({ message: "Info log!", theme: "info" });
-vc.logWithTheme({ message: "Warning log!", theme: "warn" });
-vc.logWithTheme({ message: "Error log!", theme: "error" });
